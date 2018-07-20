8e8d85bb68114b11f7d79e4f21b7e3b096ab0cbe
Galen <827472476@qq.com>
2018-07-20 11:12:35 +0800

    excel-import-第二版-可select某个单元格的字符串

---
+++ excel-import/第二版-可select某个单元格的字符串/excelTest/app/src/main/java/com/liyu/demo/User.java
@@ -0,0 +1,44 @@
+package com.liyu.demo;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Created by liyu on 2017/3/24.
+ */
+
+public class User extends LitePalSupport {
+
+    private String name;
+
+    private float price;
+
+    private byte[] cover;
+
+
+    public byte[] getCover() {
+        return cover;
+    }
+
+    public void setCover(byte[] cover) {
+        this.cover = cover;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+}
